c283aa1c3142121333616b3e97f7867e3dac6075
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-07-10 17:48:44 +0700

    Add parameters for detector

--- autonet_lane_detector/src/LaneDetector.h
+++ autonet_lane_detector/src/LaneDetector.h
@@ -16,12 +16,19 @@
 
  */
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+
 
 #ifndef SRC_LANEDETECTOR_H
 #define SRC_LANEDETECTOR_H
 
+using namespace std;
+
 /*
- * Parameter list
+ * Parameter list:
  *
  * lane_set_point   -  Lane image setpoint (px)
  * stop_line_roi    -
@@ -29,17 +36,38 @@
  * window_count     -
  * window_thresh    -
  * thresh_val       - threshold for image filter
- *
+ * lane_roi         - roi to crop
  *
  */
 
+struct LaneDetectorParams {
+    int window_size;
+    int window_count;
+    int window_thresh;
+    int thresh_val;
+    vector<cv::Point2i> lane_roi;
+    cv::Point2i  lane_setpoint;
+    vector<cv::Point2i> stop_line_roi;
+    cv::Point2i  stop_line_setpoint;
+};
 
-//class LaneDetector {
-//public:
+struct LaneDetectorOut {
+    float e1;
+    float e2;
+    cv::Point2i p1;
+    cv::Point2i p2;
+    cv::Point2i stop_line;
+    bool stop_line_bool;
+};
+
+
+class LaneDetector {
+public:
+    LaneDetector(LaneDetectorParams params);
 //    LaneDetector();
-////    LaneDetector();
-//private:
-//};
+private:
+    LaneDetectorParams params_;
+};
 
 
 #endif //SRC_LANEDETECTOR_H
--- autonet_lane_detector/src/lane_detector_node.cpp
+++ autonet_lane_detector/src/lane_detector_node.cpp
@@ -1,7 +1,8 @@
 //
 // Created by vasily on 10.07.2020.
 //
-#include <math.h>
+#include <cmath>
+#include <memory>
 #include <vector>
 #include <string>
 #include <map>
@@ -23,22 +24,32 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Polygon.h>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Point.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
-//#include <opencv2/aruco.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+
+#include <xmlrpcpp/XmlRpcValue.h>
 
 #include "LaneDetector.h"
 #include "utils.h"
 
 using cv::Mat;
+using namespace std;
 
 struct LaneDetectorNodeParams {
-
+    geometry_msgs::Polygon lane_roi;
+    string camera_frame;
+    string lane_roi_frame;
+    geometry_msgs::Point lane_setpoint;
+    geometry_msgs::Point stop_setpoint;
+    LaneDetectorParams detector_p;
 };
 
 class LaneDetectorNode{
@@ -51,17 +62,74 @@ private:
     ros::Publisher markers_pub_, vis_markers_pub_;
     ros::Subscriber map_markers_sub_;
     Mat camera_matrix_, dist_coeffs_;
+    LaneDetectorNodeParams params_;
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_priv_ = ros::NodeHandle("~");
 public:
     void onInit() {
-        ros::NodeHandle nh_;
+        ROS_INFO("LaneDetectorNode");
+//        nh_priv_ = ros::NodeHandle("~");
+
+        br_ = std::make_unique<tf2_ros::TransformBroadcaster>();
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>();
+        tf_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_, nh_);
+
         image_transport::ImageTransport it(nh_);
 //        image_transport::ImageTransport it_priv(nh_priv_);
         img_sub_ = it.subscribeCamera("image_raw", 1, &LaneDetectorNode::imageCallback, this);
         debug_pub_ = it.advertise("debug", 1);
+        getParameters();
+        projectCoordinates();
 
     }
 private:
     void getParameters(){
+        std::vector<std::string> keys;
+        nh_priv_.param("lane_roi_frame", params_.lane_roi_frame, string("base_link"));
+        nh_priv_.param("camera_frame", params_.camera_frame, string("camera_frame"));
+        nh_priv_.param("window_size", params_.detector_p.window_size, 10);
+        nh_priv_.param("window_count", params_.detector_p.window_count, 5);
+        nh_priv_.param("window_thresh", params_.detector_p.window_thresh, 50);
+        nh_priv_.param("thresh_val", params_.detector_p.thresh_val, 110);
+
+        XmlRpc::XmlRpcValue xml_lane_roi_temp;
+        nh_priv_.getParam("lane_roi", xml_lane_roi_temp);
+        if( xml_lane_roi_temp.getType() != XmlRpc::XmlRpcValue::TypeArray ) {
+            ROS_ERROR("param 'xml_lane_roi_temp' is not a list");
+        } else {
+            for( int i=0; i<xml_lane_roi_temp.size(); ++i ) {
+                if( xml_lane_roi_temp[i].getType() != XmlRpc::XmlRpcValue::TypeArray ) {
+                    ROS_ERROR("xml_lane_roi_temp[%d] is not a list", i);
+                } else {
+                    if( xml_lane_roi_temp[i].size() != 3 ) {
+                        ROS_ERROR("xml_lane_roi_temp[%d] size not 3 ", i);
+                    }
+                    else if(
+                            xml_lane_roi_temp[i][0].getType() != XmlRpc::XmlRpcValue::TypeDouble ||
+                            xml_lane_roi_temp[i][1].getType() != XmlRpc::XmlRpcValue::TypeDouble ||
+                            xml_lane_roi_temp[i][2].getType() != XmlRpc::XmlRpcValue::TypeDouble ){
+                        ROS_ERROR("xml_lane_roi_temp[%d] is not double", i);
+                    }
+                    else {
+                        geometry_msgs::Point32 pnt;
+
+                        pnt.x = static_cast<double>(xml_lane_roi_temp[i][0]);
+                        pnt.y = static_cast<double>(xml_lane_roi_temp[i][1]);
+                        pnt.z = static_cast<double>(xml_lane_roi_temp[i][2]);
+                        params_.lane_roi.points.push_back(pnt);
+                    }
+                }
+            }
+        }
+        cout << params_.lane_roi << endl;
+//        nh_priv_.param("lane_roi", lane_roi_temp);
+//        for (int i = 0; i < lane_roi_temp.size(); i++){
+//            geometry_msgs::Point32 pnt;
+//            pnt.x = lane_roi_temp[i][0];
+//            pnt.y = lane_roi_temp[i][1];
+//            pnt.z = lane_roi_temp[i][2];
+//            params_.lane_roi.points.push_back(pnt);
+//        }
 
     }
     void projectCoordinates(){
@@ -72,6 +140,7 @@ private:
         parseCameraInfo(cinfo, camera_matrix_, dist_coeffs_);
         Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
         Mat out_image = image.clone();
+
 
 
 
@@ -89,8 +158,9 @@ private:
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "autonet_lane_detector");
-
-
+    LaneDetectorNode ldn;
+    ldn.onInit();
+    ros::spin();
     return 0;
 }
 
